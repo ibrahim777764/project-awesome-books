ed161a54b11a60f1b6df5191255cee3e426e784e
efoosag <96612103+efoosag@users.noreply.github.com>
2022-05-23 13:53:37 +0100

    add functionality for insert book

---
+++ index.js
@@ -0,0 +1,47 @@
+// let disAuthor = document.querySelector('.author-input').value;
+// let disTitle = document.querySelector('title-input').value;
+const add = document.querySelector('.add-button');
+const remove = document.querySelector('#remove');
+const ul = document.querySelector('.book-list');
+
+const display_books = [
+  { title: 'Wonderland', author: 'James Brown' },
+  { title: 'Night Crawler', author: 'Charles Chase' },
+];
+
+add.addEventListener('click', (e) => {
+  display_books.title = document.getElementById('title-input').value;
+  display_books.author = document.getElementById('author-input').value;
+  display_books.push({
+    title: display_books.title,
+    auhtor: display_books.author,
+  });
+  bookTitle = display_books.title;
+  bookAuthor = display_books.author;
+  const display_content = `<p class="display_title">${bookTitle}</p>
+  <p class="display_author">${bookAuthor}</p>
+  <button type="button" id="remove">remove</button>`;
+
+  const li = document.createElement('li');
+  li.classList.add('bbline');
+  li.innerHTML = display_content;
+  ul.append(li);
+  console.log(display_books);
+});
+
+let bookTitle = '';
+let bookAuthor = '';
+
+display_books.forEach((book) => {
+  bookTitle = book.title;
+  bookAuthor = book.author;
+
+  const display_content = `<p class="display_title">${bookTitle}</p>
+<p class="display_author">${bookAuthor}</p>
+<button type="button" id="remove">remove</button>`;
+
+  const li = document.createElement('li');
+  li.classList.add('bbline');
+  li.innerHTML = display_content;
+  ul.append(li);
+});
