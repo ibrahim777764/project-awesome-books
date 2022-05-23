0e592ff3e97beee14b9fca4f73dcbc7a68aae3f9
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-05-23 18:19:20 +0500

    js file

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 /* eslint-disable max-classes-per-file */
 
-const listLink = document.getElementById("list");
-const formLink = document.getElementById("add-new");
-const contactLink = document.getElementById("contact");
+const listLink = document.getElementById('list');
+const formLink = document.getElementById('add-new');
+const contactLink = document.getElementById('contact');
 
-const list = document.getElementById("list-section");
-const formSection = document.getElementById("add-new-section");
-const contactSection = document.getElementById("contact-section");
+const list = document.getElementById('list-section');
+const formSection = document.getElementById('add-new-section');
+const contactSection = document.getElementById('contact-section');
 
-formSection.style.display = "none";
-contactSection.style.display = "none";
+formSection.style.display = 'none';
+contactSection.style.display = 'none';
 
-listLink.addEventListener("click", () => {
-  formSection.style.display = "none";
-  contactSection.style.display = "none";
-  list.style.display = "block";
+listLink.addEventListener('click', () => {
+  formSection.style.display = 'none';
+  contactSection.style.display = 'none';
+  list.style.display = 'block';
 });
 
-formLink.addEventListener("click", () => {
-  formSection.style.display = "block";
-  contactSection.style.display = "none";
-  list.style.display = "none";
+formLink.addEventListener('click', () => {
+  formSection.style.display = 'block';
+  contactSection.style.display = 'none';
+  list.style.display = 'none';
 });
 
-contactLink.addEventListener("click", () => {
-  formSection.style.display = "none";
-  contactSection.style.display = "block";
-  list.style.display = "none";
+contactLink.addEventListener('click', () => {
+  formSection.style.display = 'none';
+  contactSection.style.display = 'block';
+  list.style.display = 'none';
 });
 
 // eslint-disable-next-line max-classes-per-file
@@ -42,11 +42,11 @@ class BookStore {
   constructor(initialData = []) {
     this.saveToLocalStorage = (data) => {
       const booksString = JSON.stringify(data);
-      localStorage.setItem("bookStoreData", booksString);
+      localStorage.setItem('bookStoreData', booksString);
       return true;
     };
 
-    const rawBooksData = localStorage.getItem("bookStoreData");
+    const rawBooksData = localStorage.getItem('bookStoreData');
     if (rawBooksData) {
       this.books = JSON.parse(rawBooksData);
     } else {
@@ -80,8 +80,8 @@ class BookStore {
 // 2. creates a li element and populates the objects with it
 // 3. query the ul element and appends the li to it
 const displayBook = ({ title, author, id }, parentElement) => {
-  const bookListItemElement = document.createElement("li");
-  bookListItemElement.className = "book-list-item";
+  const bookListItemElement = document.createElement('li');
+  bookListItemElement.className = 'book-list-item';
   bookListItemElement.innerHTML = `
       <section class="book-store-section display-flex">
         <div class="display-flex">
@@ -98,19 +98,19 @@ const generateId = () => `id_${Math.random().toString(36).slice(2)}`;
 
 const initialBooks = [
   {
-    title: "the boy with wings",
-    author: "Basit Korede",
+    title: 'the boy with wings',
+    author: 'Basit Korede',
     id: generateId(),
   },
 
   {
-    title: "Think Pythone",
-    author: "korede Basit",
+    title: 'Think Pythone',
+    author: 'korede Basit',
     id: generateId(),
   },
 ];
 
-const bookListElement = document.querySelector("ul.book-list");
+const bookListElement = document.querySelector('ul.book-list');
 
 const bookStore = new BookStore(initialBooks);
 const books = bookStore.all();
@@ -118,22 +118,22 @@ books.forEach((book) => {
   displayBook(book, bookListElement);
 });
 
-const formElement = document.querySelector("#book-form");
+const formElement = document.querySelector('#book-form');
 const handleSubmition = (event) => {
   event.preventDefault();
-  const title = document.querySelector(".title-input").value;
-  const author = document.querySelector(".author-input").value;
+  const title = document.querySelector('.title-input').value;
+  const author = document.querySelector('.author-input').value;
   const id = generateId();
-  const clearField = document.querySelectorAll("input");
+  const clearField = document.querySelectorAll('input');
   clearField.forEach((input) => {
-    input.value = "";
+    input.value = '';
   });
   const newBook = new Book({ title, author, id });
   if (bookStore.add(newBook)) {
     displayBook(newBook, bookListElement);
   }
 };
-formElement.addEventListener("submit", handleSubmition);
+formElement.addEventListener('submit', handleSubmition);
 
 // eslint-disable-next-line no-unused-vars
 const handleRemove = (currentId) => {
@@ -144,24 +144,22 @@ const handleRemove = (currentId) => {
 };
 
 const navigatePage = () => {
-  const navLists = document.querySelectorAll(".nav-list-item");
+  const navLists = document.querySelectorAll('.nav-list-item');
   const handleNavigation = (event) => {
     //
-    if (event.target.classList.contains("active")) {
+    if (event.target.classList.contains('active')) {
       return false;
     }
 
-    document.querySelector(".nav-list-item.active").classList.remove("active");
-    event.target.classList.add("active");
-    document.querySelector("section.active").classList.remove("active");
-    document
-      .querySelector(`#${event.target.id}-section`)
-      .classList.add("active");
+    document.querySelector('.nav-list-item.active').classList.remove('active');
+    event.target.classList.add('active');
+    document.querySelector('section.active').classList.remove('active');
+    document.querySelector(`#${event.target.id}-section`).classList.add('active');
     return true;
   };
 
   navLists.forEach((navlist) => {
-    navlist.addEventListener("click", handleNavigation);
+    navlist.addEventListener('click', handleNavigation);
   });
 };
 navigatePage();
